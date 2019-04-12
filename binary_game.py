5b7ca56f09f2ca1798417813ef7a386228832bb6
Drew <andrewfellenz@gmail.com>
2019-04-12 00:44:33 -0400

    Changed the name of utils.py to binary_game.py. The game is starting to function.

--- binary_game.py
+++ binary_game.py
@@ -1,123 +1,71 @@
-import random
 import os
+import random
 import time
 import datetime
 
 
-numbers_to_guess = [random.randint(0, 255) for num in range(10)]
+# This file will continue the classes used for the games
+# This is the parent class for the games
+class Game:
+    def __init__(self, time=False, bits=8, high_score=0):
+        self.time = time
+        self.bits = bits
+        self.high_score = high_score
 
-def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    time.sleep(.5)
 
-
-# Prompts the player to choose whether they want to be timed or not
-# Also prompts the player to select the number of bits they want to
-# attempt to guess
-def bits_time():
-
-    time_message = ("Timed games have 10 questions.\n"
-                    "Would you like to play a timed game?"
-                    "[Yes/No] ")
-
-    bits_message = ("Enter the number of "
-                   "bits you would like to guess.\n"
-                   "The number must be a multiple of 2.\n"
-                   "If you leave this box blank, "
-                   "the game will default to 8 bits.\n"
-                   "The minimum and maximum selectable "
-                   "numbers are 2 and 64. ")
-
-    while True:
-        try:
-            time = input(time_message).lower()
-
-            clear_screen()
-            if time == "y" or time == "yes":
-                time = True
-                break
-            elif time == "n" or time == "no":
-                time = False
-                break
-            else:
-                clear_screen()
-                raise ValueError('Please type "Yes" or "No".')
-        except ValueError as error:
-            print(error)
-
-    while True:
-        try:
-            bits = int(input(bits_message))
-
-        except ValueError:
-            num_message = ("You must select a number between 2 and 64! ")
-            clear_screen()
-            continue
+    # This property dynamically determines if there is a timer or not
+      # This property needs to be modified to actually BE the timer
+    @property
+    def timer(self):
+        if self.time == True:
+            print("@property time is true")
+            # Get current date time as starting point
+            # Routinely check datetime to make sure
+              # that the game isn't out of time
+            
         else:
-            clear_screen()
-            if bits % 2 == 0 and bits < 66 and bits > 0:
-                return bits, time
+            print("No time limit")
+
+
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    
+    # This will be the code to convert numbers to binary or hex
+    def get_nums(self):
+        while True:
+            number_set = set([random.randint(0, 15) for _ in range(10)])
+            if len(number_set) < 10:
+                continue
             else:
-                print("You must select a number between 2 and 64! ")
-
-
-# This is the game mode for guessing binary numbers: 01010101
-def binary_mode(bits, time):
-    if time == True:
-        pass
-    else:
-        pass
+                guess_list = [num for num in number_set]
+                return guess_list
+                
 
     
+    # Converts a random number into a plain binary Str for comparison
+    def convert(self, num):
+        converted_num = str(bin(num))
+        answer_num = [str(num) for num in converted_num[2:]]
+        while len(answer_num) < self.bits:
+            answer_num.insert(0, "0")
+        return ''.join(answer_num)
+           
 
-def hex_mode(bits, time):
-    pass
-
-# Starts the game and prompts user to select a mode.
-def start_game():
-
-    clear_screen()
-
-    welcome_message = "Welcome to the Binary/Hexadecimal Speed Testing Game!"
-
-    print(welcome_message)
-
-    input('Press "Enter" to continue!')
-
-    clear_screen()
-    while True:
-        try:
-            game_mode = input("Would you like to play in [b]inary or [h]ex mode? ").lower()
-            
-            clear_screen()
-            
-            if game_mode == "binary" or game_mode == "b":
-                binary_mode(*bits_time())
-                break
-            
-            elif game_mode == "hex" or game_mode == "h":
-                hex_mode(*bits_time())
-                break
-
-            else:
-                raise ValueError('Please type in "hex" or "binary".')
-        except ValueError as err:
-            print(err)
-    
-    
-
-    
-
-start_game()
-
-
-
-# Have a timer increment time
-
-# Have a score board and a high score option available
-
-# Save a record to a .json file of the high score (sorted by hex and binary)
-
-# Have a reverse mode for inputting base 10 numbers to guess binary / hex numbers
-
-# consider incorporating ASCII codes as well
+    def play(self):
+        nums = self.get_nums()
+        count = 0
+        score = 0
+        while count < 10:
+            self.clear_screen()
+            print("Current score:{}\n".format(score))
+            current_number = random.choice(nums)
+            nums.remove(current_number)
+            guess = input("What is {} in binary? ".format(current_number))
+            correct_answer = self.convert(current_number)
+            if guess == correct_answer:
+                score += 1
+            count += 1
+           
+game = Game()
+game.play()
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
-import datetime
+import os
 import random
+import time
+import datetime
+
 
 # This file will continue the classes used for the games
 # This is the parent class for the games
@@ -22,30 +25,47 @@ class Game:
             
         else:
             print("No time limit")
+
+
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
     
     # This will be the code to convert numbers to binary or hex
     def get_nums(self):
         while True:
-            guess_numbers = set([random.randint(0, 255) for _ in range(10)])
-            if len(guess_numbers) < 10:
+            number_set = set([random.randint(0, 15) for _ in range(10)])
+            if len(number_set) < 10:
                 continue
             else:
-                return guess_numbers
+                guess_list = [num for num in number_set]
+                return guess_list
+                
 
     
     # Converts a random number into a plain binary Str for comparison
     def convert(self, num):
         converted_num = str(bin(num))
-        answer_num = [num for num in converted_num[2:]]
+        answer_num = [str(num) for num in converted_num[2:]]
         while len(answer_num) < self.bits:
             answer_num.insert(0, "0")
         return ''.join(answer_num)
            
 
     def play(self):
-        nums = get_nums()
+        nums = self.get_nums()
         count = 0
-        while True count < 10:
-
-            
-e = Game()
+        score = 0
+        while count < 10:
+            self.clear_screen()
+            print("Current score:{}\n".format(score))
+            current_number = random.choice(nums)
+            nums.remove(current_number)
+            guess = input("What is {} in binary? ".format(current_number))
+            correct_answer = self.convert(current_number)
+            if guess == correct_answer:
+                score += 1
+            count += 1
+           
+game = Game()
+game.play()
